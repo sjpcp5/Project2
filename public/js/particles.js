547bbfbd7657cb6ad5ef18493c870841e789b773
Andrew Myers <ajmyers5@gmail.com>
2020-03-21 10:41:21 -0500

    particles

--- public/js/particles.js
+++ public/js/particles.js
@@ -1,20 +1,20 @@
 particlesJS("particles-js", {
     particles: {
-      number: { value: 80, density: { enable: true, value_area: 800 } },
-      color: { value: "#ffffff" }, //color of particle - #8db8b3 greeny-grey
+      number: { value: 30, density: { enable: true, value_area: 800 } },
+      color: { value: "#8db8b3" }, //color of particle - #8db8b3 greeny-grey
       shape: {
-        type: "circle", //shape of particle
+        type: "polygon", //shape of particle
         stroke: { width: 1, color: "#ad0725" },//border color of particle and color #ad0725=>red
-        polygon: { nb_sides: 5 },
+        polygon: { nb_sides: 8 },
         image: { src: "img/github.svg", width: 100, height: 100 }
       },
       opacity: {
-        value: 0.5, //opacity of particle
+        value: 0.6, //opacity of particle
         random: false,
         anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
       },
       size: {
-        value: 3, //size of particle
+        value: 10, //size of particle
         random: true,
         anim: { enable: false, speed: 40, size_min: 0.1, sync: false }
       },
@@ -27,9 +27,9 @@ particlesJS("particles-js", {
       },
       move: {
         enable: true, //true = move, false = stationary
-        speed: 6, //speed of particles
+        speed: 3, //speed of particles
         direction: "none",
-        random: false,
+        random: true,
         straight: false,
         out_mode: "out",
         bounce: false,
@@ -46,7 +46,7 @@ particlesJS("particles-js", {
       modes: {
         grab: { distance: 400, line_linked: { opacity: 1 } },
         bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
-        repulse: { distance: 200, duration: 0.4 },
+        repulse: { distance: 25, duration: 0.1 },
         push: { particles_nb: 4 },
         remove: { particles_nb: 2 }
       }
