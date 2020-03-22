cdaaeabb2a6a21c31e13fb63c4257cc045a06077
Cutie on the Couch <sjpcp5@gmail.com>
2020-03-22 13:42:00 -0500

    updated models, created HTML routes, added public and db to gitignore
    removed some linter problems
    
    ran npm i -d eslint

--- db/schema.sql
+++ db/schema.sql
@@ -1,33 +1,30 @@
-CREATE DATABASE IF NOT EXISTS coolBeans_db;
+CREATE DATABASE coolBeans_db;
 
 USE coolBeans_db;
 
-DROP TABLE IF EXISTS words;
-DROP TABLE IF EXISTS scores;
-DROP TABLE IF EXISTS users;
+-- DROP TABLE IF EXISTS words;
+-- DROP TABLE IF EXISTS scores;
+-- DROP TABLE IF EXISTS users;
 
-CREATE TABLE words (
-  id int NOT NULL AUTO_INCREMENT,
-  difficulty VARCHAR(15) NOT NULL,
-  words VARCHAR(25) NOT NULL,
-  letter_count INTEGER,
-  PRIMARY KEY (id)
-);
+-- CREATE TABLE words (
+--   id int NOT NULL AUTO_INCREMENT,
+--   difficulty VARCHAR(15) NOT NULL,
+--   words VARCHAR(25) NOT NULL UNIQUE,
+--   letterCount INTEGER,
+--   PRIMARY KEY (id)
+-- );
 
-CREATE TABLE scores (
-  id int NOT NULL AUTO_INCREMENT,
-  player VARCHAR(50) NOT NULL,
-  score INTEGER NOT NULL,
-  PRIMARY KEY (id)
-);
+-- CREATE TABLE scores (
+--   id int NOT NULL AUTO_INCREMENT,
+--   player VARCHAR(50) NOT NULL,
+--   score INTEGER NOT NULL,
+--   PRIMARY KEY (id)
+-- );
 
-CREATE TABLE users (
-  id int NOT NULL AUTO_INCREMENT,
-  userRole VARCHAR(50) NOT NULL,
-  userName VARCHAR(50) NOT NULL,
-  pword VARCHAR(50) NOT NULL,
-  PRIMARY KEY (id)
-);
-
-
-
+-- CREATE TABLE users (
+--   id int NOT NULL AUTO_INCREMENT,
+--   userRole VARCHAR(50) NOT NULL,
+--   email VARCHAR(50) NOT NULL UNIQUE,
+--   pword VARCHAR(50) NOT NULL,
+--   PRIMARY KEY (id)
+-- );
---
+++ models/words.js
@@ -0,0 +1,39 @@
+module.exports = function(sequelize, DataTypes) {
+    var Words = sequelize.define("words", {
+        // The email cannot be null, and must be a proper email before creation
+        difficulty: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isIn: [
+                    ['easy', 'medium', 'hard']
+                ],
+            },
+        },
+        words: { // user name  cannot be null and must be unique
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isAlpha: true,
+                len: [1, 20],
+            },
+
+        },
+        // The password cannot be null
+        letterCount: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+
+        },
+    }, {
+        freezeTableName: true, //model table name will be the same as the model name 
+
+    });
+    Words.beforeUpsert(async(words, options) => {
+        const phraseLength = await words.split(" ").filter(c => /\w/.test(c)).length;
+        words.letterCount = phraseLength;
+    })
+
+    return Words;
+};
