1fa4402770586e3fa8649360f75503fa593c20bd
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-03-23 19:39:03 -0500

    Syncing Saphirah's model from the server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+const db = require("./models");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -10,10 +11,12 @@ app.use(express.static("public"));
 
 // require("./routes/html-routes")(app);
 
-app.listen(PORT, () => {
-  console.log(
-    "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-    PORT,
-    PORT
-  );
+db.sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
 });
