a32eafc47af05a90b1d3ed20902c0a5532dbbcb3
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-03-23 19:21:24 -0500

    Adding node module dependencies and server code

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// require("./routes/html-routes")(app);
+
+app.listen(PORT, () => {
+  console.log(
+    "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+    PORT,
+    PORT
+  );
+});
