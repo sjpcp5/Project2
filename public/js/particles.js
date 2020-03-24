2617607cc664c60203c6e071a1ccec721439e6e4
Christopher Wood <cjwood01@gmail.com>
2020-03-24 18:23:01 -0500

    cleaned up input space. tweaked colors.

---
+++ public/js/particles.js
@@ -0,0 +1,58 @@
+
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 30, density: { enable: true, value_area: 800 } },
+    color: { value: "#80cbc4" }, //color of particle - #8db8b3 greeny-grey
+    shape: {
+      type: "polygon", //shape of particle
+      stroke: { width: 1, color: "#ad0725" },//border color of particle and color #ad0725=>red
+      polygon: { nb_sides: 8 },
+      image: { src: "img/github.svg", width: 100, height: 100 }
+    },
+    opacity: {
+      value: 0.6, //opacity of particle
+      random: false,
+      anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
+    },
+    size: {
+      value: 10, //size of particle
+      random: true,
+      anim: { enable: false, speed: 40, size_min: 0.1, sync: false }
+    },
+    line_linked: {
+
+      enable: false, //true keeps line, false gets rid of line.  must leave
+      distance: 150,
+      color: "#ffffff",
+      opacity: 0.4,
+      width: 1
+    },
+    move: {
+      enable: true, //true = move, false = stationary
+      speed: 3, //speed of particles
+      direction: "none",
+      random: true,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: { enable: false, rotateX: 600, rotateY: 1200 }
+    }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: { enable: true, mode: "repulse" },
+      onclick: { enable: true, mode: "push" },
+      resize: true
+    },
+    modes: {
+      grab: { distance: 400, line_linked: { opacity: 1 } },
+      bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
+      repulse: { distance: 20, duration: 0.1 },
+      push: { particles_nb: 4 },
+      remove: { particles_nb: 2 }
+    }
+  },
+  retina_detect: true
+});
+
