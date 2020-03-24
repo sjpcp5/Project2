0119d72f28e16bd38032222b96e27930a66ade5e
saphirah.pociluyko <saphirah.j.pociluyko@usace.army.mil>
2020-03-23 20:25:14 -0500

    initial commit

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,6 +2,7 @@ INSERT INTO words (difficulty, words, letter_count)
 VALUES ("easy","COVID19"),("easy","Washing"),("medium","distancing"),("easy","cough"),("easy","fever"),("easy","tiredness"),("easy","breathing"),("easy","disease"),("easy","healthy"),("medium","developing"),("medium","coronavirus"),("meduim","conditions"),("medium","vulnerable"),("medium","caboodling"),("easy","trickle"),
 ("medium","awesomeness"),("medium","exercising"),("medium","drinking water"),("medium","eating soup"),("hard","gargling saltwater"),("hard","milder symptoms"),("medium","eat vegetables"),("medium","eating fruits"),("hard","productive cough"),("hard","nasuea and diarrhea"),("hard","incubation period"),("medium","sleeping well")
 ,("hard","essential personnel"),("medium","Mandalorian"),("hard","Stay Calm and Carry On"),("hard","world mythologies"),("hard","tequila junction");
+
 /* more than 15 hard, more than 10 easy, less than 10 easy */
 
 /* INSERT INTO users (userRole, email, pword)
