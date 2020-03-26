606d39d8995fd337bc0ebb99b9335e162f0bff17
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-03-26 18:43:57 -0500

    Modifying game function to add the highscore functionality

--- models/scores.js
+++ models/scores.js
@@ -1,29 +1,26 @@
 module.exports = function(sequelize, DataTypes) {
-  var Scores = sequelize.define("scores", {
-    // The email cannot be null, and must be a proper email before creation
-    player: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        isAlphanumeric: [
-          true
-        ],
+  var Scores = sequelize.define(
+    "Scores",
+    {
+      // The email cannot be null, and must be a proper email before creation
+      player: {
+        type: DataTypes.STRING,
+        allowNull: false
       },
+      score: {
+        // user name  cannot be null and must be unique
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          isNumeric: true,
+          len: [1, 1000]
+        }
+      }
     },
-    score: { // user name  cannot be null and must be unique
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      validate: {
-        isNumeric: true,
-        len: [1, 1000],
-      },
-
-    },
-
-  }, {
-
-    freezeTableName: true //model table name will be the same as the model name
-  });
+    {
+      freezeTableName: true //model table name will be the same as the model name
+    }
+  );
 
   return Scores;
 };
