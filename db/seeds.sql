2e518769d0df8b037de237abc126371d011918f1
Christopher Wood <cjwood01@gmail.com>
2020-03-26 20:31:34 -0500

    added more words to seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,12 @@
 INSERT INTO words (difficulty, words, letter_count)
 VALUES ("easy","COVID19"),("easy","Washing"),("medium","distancing"),("easy","cough"),("easy","fever"),("easy","tiredness"),("easy","breathing"),("easy","disease"),("easy","healthy"),("medium","developing"),("medium","coronavirus"),("meduim","conditions"),("medium","vulnerable"),("medium","caboodling"),("easy","trickle"),
 ("medium","awesomeness"),("medium","exercising"),("medium","drinking water"),("medium","eating soup"),("hard","gargling saltwater"),("hard","milder symptoms"),("medium","eat vegetables"),("medium","eating fruits"),("hard","productive cough"),("hard","nasuea and diarrhea"),("hard","incubation period"),("medium","sleeping well")
-,("hard","essential personnel"),("medium","Mandalorian"),("hard","Stay Calm and Carry On"),("hard","world mythologies"),("hard","tequila junction");
+,("hard","essential personnel"),("medium","Mandalorian"),("hard","Stay Calm and Carry On"),("hard","world mythologies"),("hard","tequila junction"),("easy", "lockbox"),("easy", "jayhawk"),("easy", "maximize"),("easy", "highjack"),("easy", "grizzly"),("medium", "github sucks"),("easy", "xenophile"),("easy", "hemolymph"),("easy", "sartorius"),("medium", "acetabulum"),("medium", "inflammation"),("medium", "contamination"),("medium", "contamination"),("medium", "sanitation"),("medium", "deterioration"),("medium", "regeneration"),("hard", "Hippopotomonstrosesquippedaliophobia"),("hard", "Acute pancreatitis"),("hard", "sesquipedalian"),("hard", "supercalifragilisticexpialidocious"),("hard", "incomprehensibility");
+/* more than 15 hard, more than 10 easy, less than 10 easy */
+
+/* INSERT INTO users (userRole, email, pword)
+VALUES (" ") */
+;
 /* more than 15 hard, more than 10 easy, less than 10 easy */
 
 /* INSERT INTO users (userRole, email, pword)
